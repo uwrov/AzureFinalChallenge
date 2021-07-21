0c31c9b19cb8c6ed16756fc22eb754ac525d33ae
Andrew <anemnox@gmail.com>
2021-07-20 20:05:20 -0700

    Added basic layout of UI and started file management

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,15 +1,14 @@
-
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow} = require('electron');
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    'minWidth': 800,
+    'minHeight': 600
   })
 
   win.loadFile('public/index.html');
-  console.log("hlloooeooeoeoeoel");
-  console.log(document.getElementById("hello"));
 }
 
 app.whenReady().then(() => {
