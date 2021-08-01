a57d8bfa12789f7d30fd283c10b5f66cbfc86286
Andrew <anemnox@gmail.com>
2021-08-01 01:48:54 -0700

    updated ui

--- fishui/src/components/mainui.js
+++ fishui/src/components/mainui.js
@@ -3,7 +3,8 @@ import "../index.css";
 
 import DropDown from "./dropdown";
 import VideoSection from "./videosection";
-import Options from "./options"
+import Options from "./options";
+import DataDisplay from "./dataDisplay";
 
 const SERVER_PORT = "4040";
 const LOCAL_HOST = "localhost";
@@ -12,6 +13,38 @@ const DEFAULT_URL = LOCAL_HOST;
 
 const LOCAL_SOCKET = generateSocket(LOCAL_HOST);
 const REMOTE_SOCKET = generateSocket(REMOTE_HOST);
+
+
+const TEST = {
+    "fish type": {
+        "0": "s-major",
+        "1": "s-major",
+        "2": "s-major",
+        "18": "s-major",
+        "19": "s-major",
+        "20": "s-major",
+        "22": "s-major",
+        "29": "s-major",
+        "31": "s-major",
+        "32": "s-major",
+        "33": "s-major",
+        "35": "s-major"
+    },
+    "timestamp": {
+        "0": "0:00",
+        "1": "0:00",
+        "2": "0:01",
+        "18": "0:06",
+        "19": "0:06",
+        "20": "0:06",
+        "22": "0:07",
+        "29": "0:07",
+        "31": "0:08",
+        "32": "0:08",
+        "33": "0:08",
+        "35": "0:09"
+    }
+}
 
 function generateSocket(domain) {
   return require("socket.io-client")("http://" + domain + ":" + SERVER_PORT);
@@ -23,7 +56,9 @@ export default class MainUI extends React.Component {
     isLocal: true,
     video: null,
     fishes: [],
-    resultVideos: []
+    resultVideos: [],
+    resultDatas: [],
+    resultIndex: -1
   }
 
   render() {
@@ -31,8 +66,9 @@ export default class MainUI extends React.Component {
       <div className="main-body">
         <div className="navbar">
           <DropDown setVideo={this.setVideo} />
-          <input className="url-input" type="button" onClick={this.toggleConnection}
-              value={this.state.isLocal ? "Local Connection" : "Remote Connection"}/>
+          <button className="url-input" onClick={this.toggleConnection}>
+            {this.state.isLocal ? "Local Connection" : "Remote Connection"}
+          </button>
           <button className="analyze-button" onClick={this.uploadVideo}>Analyze</button>
         </div>
         <div className="content">
@@ -40,13 +76,50 @@ export default class MainUI extends React.Component {
           <Options setFishes={this.setFishes}/>
           </div>
           <div className="videos">
-            <VideoSection video={this.state.video} />
+            <div className="tab-section">{this.renderTabs()}</div>
+            {this.renderVideo()}
           </div>
           <div className="details">
+            {this.renderData()}
           </div>
         </div>
       </div>
     );
+  }
+
+  renderTabs() {
+      const tabs = [];
+      if(this.state.video != null) {
+        tabs.push((<div className="video-tab" onClick={() => this.setTab(-1)}>Original</div>))
+      }
+      tabs.push(this.state.resultVideos.map((k, i) => {
+        return (
+          <div className="video-tab" onClick={() => this.setTab(i)}>Analysis {i}</div>
+        )
+      }));
+  }
+
+  renderVideo() {
+      if(this.state.resultIndex === -1) {
+        return <VideoSection video={this.state.video} />
+      } else {
+        return <VideoSection video={this.state.resultVideos[this.state.resultIndex]} />
+      }
+  }
+
+  renderData() {
+    if (true) { //this.state.resultIndex >= 0) {
+      return (
+        <DataDisplay data={TEST//this.state.resultDatas[this.state.resultIndex]
+        } />
+      );
+    } else {
+      return (<div className="data-display">No Data</div>);
+    }
+  }
+
+  setTab = (index) => {
+    this.setState({resultIndex: index});
   }
 
   setVideo = (video) => {
